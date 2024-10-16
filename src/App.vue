6ba3ef65ece10901102ac9a90aa2aead35d8f4db
victorcngo <hello@victorcango.fr>
2024-10-16 17:49:51 +0200

    chore: setup

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,26 @@
 	import { RouterView } from "vue-router"
 	import { onMounted, onBeforeUnmount, provide, shallowRef } from "vue"
 	import { useRaf } from "@/composables/useRaf/useRaf"
+	import { Game } from "@/game/Game"
 
 	// Game state
 	let game = shallowRef()
+	let t = 0
 	provide("game", game)
 
 	useRaf((dt) => {
 		if (!game.value) return
 		t += dt
-		// game.value.update(ctx, dt, t)
-		// game.value.draw(ctx, dt, t)
+		game.value.update(dt, t)
 	})
 
 	onMounted(() => {
-		// game.value = new Game()
-		// game.value.setup()
+		game.value = new Game()
+		game.value.setup()
 	})
 
 	onBeforeUnmount(() => {
-		// game.value.destroy()
+		game.value.destroy()
 	})
 </script>
 
--- src/game/Game.js
+++ src/game/Game.js
@@ -1,3 +1,13 @@
 export class Game {
-	// ...
+	setup() {
+		console.log("Game setup")
+	}
+
+	update(dt) {
+		console.log("Game update")
+	}
+
+	destroy() {
+		console.log("Game destroy")
+	}
 }
