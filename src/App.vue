7d6c3044f5d8c0d54759a9ede9e6c9c787e117ef
victorcngo <hello@victorcango.fr>
2024-10-16 17:46:36 +0200

    feat: setup game raf

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,27 @@
 <script setup>
 	import { RouterView } from "vue-router"
+	import { onMounted, onBeforeUnmount, provide, shallowRef } from "vue"
+	import { useRaf } from "@/composables/useRaf/useRaf"
+
+	// Game state
+	let game = shallowRef()
+	provide("game", game)
+
+	useRaf((dt) => {
+		if (!game.value) return
+		t += dt
+		// game.value.update(ctx, dt, t)
+		// game.value.draw(ctx, dt, t)
+	})
+
+	onMounted(() => {
+		// game.value = new Game()
+		// game.value.setup()
+	})
+
+	onBeforeUnmount(() => {
+		// game.value.destroy()
+	})
 </script>
 
 <template>
