61c6c60021f124e6b18bcae1a0e6150064b15488
Victor-Sin <80609577+Victor-Sin@users.noreply.github.com>
2024-10-17 00:26:05 +0200

    Player : Initialize

--- src/game/Game.js
+++ src/game/Game.js
@@ -1,13 +1,21 @@
 import PixiApplication from "./pixi/PixiApplication"
 import PixiSprite from "./pixi/PixiSprite"
+import InputSet from "./InputSet";
+import Player from "@/game/player/Player";
 
 export class Game {
+	static instance
 	isPaused = false
 	soundManager = null
 	existingIngredientList = {}
 	stationsList = []
 
 	constructor(canvasWrapper) {
+		if(Game.instance){
+			return Game.instance
+		}
+
+		Game.instance = this
 		this.canvasWrapper = canvasWrapper
 	}
 
@@ -25,21 +33,27 @@ export class Game {
 	}
 
 	setup() {
-		this.pixiApplication = new PixiApplication(this.canvasWrapper)
+		InputSet.emulateKeyboard()
+		InputSet.emulateGamePad()
+		InputSet.initPlayersInputs()
+		this.pixiApplication = new PixiApplication()
 		this.initApplication(this.pixiApplication).then(async () => {
 			// Objects
-			this.pixiSprite = new PixiSprite("https://pixijs.com/assets/bunny.png")
-			await this.pixiSprite.init().then((sprite) => {
-				this.pixiApplication.appendToStage(sprite)
-			})
+			this.playerA = new Player(1,"https://pixijs.com/assets/bunny.png")
+			await this.playerA.initPixiSprite()
+			this.playerA.addInputsListener()
 		})
 		console.log("Game setup")
 	}
 
 	update(dt, t) {
-		console.log("Game update")
+		// console.log("Game update")
+		InputSet.update()
 		if (this.pixiSprite) {
 			this.pixiSprite.update(t)
+		}
+		if(this.playerA) {
+			this.playerA.update(dt,t)
 		}
 	}
 
