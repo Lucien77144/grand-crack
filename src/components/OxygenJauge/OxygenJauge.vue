150db2eb65eb5265266c279ae352504e4e4f4a1b
Amandine Quellec <quellec.amandine@gmail.com>
2024-10-18 00:47:43 +0200

    feat(oxygen): add ui

--- src/components/OxygenJauge/OxygenJauge.vue
+++ src/components/OxygenJauge/OxygenJauge.vue
@@ -1,5 +1,6 @@
 <script setup>
 	import { inject, computed } from "vue"
+	import oxygenIcon from "/assets/ui/oxygen/oxygen-icon.svg"
 
 	const props = defineProps({
 		player: {
@@ -18,52 +19,78 @@
 
 <template>
 	<div
-		class="oxygen-jauge"
-		:class="
-			'player-' + player
-		"
+		class="oxygen-container"
 	>
 		<div
-			class="progress"
-			:style="
-				{
-					transform: 'scaleY(' + oxygen / 100 + ')',
-				}
+			class="oxygen-jauge"
+			:class="
+				'player-' + player
 			"
-		/>
+		>
+			<div
+				class="progress"
+				:style="
+					{
+						transform: 'scaleY(' + oxygen / 100 + ')',
+					}
+				"
+			/>
+		</div>
+
+		<img
+			class="oxygen-icon"
+			:src="oxygenIcon"
+			:alt="'Oxygène du joueur ' + player"
+		>
 	</div>
 </template>
 
 <style lang="scss" scoped>
-	.oxygen-jauge {
-		border-radius: 1rem;
-		height: 15rem;
-		inset: 0.05rem solid white;
-		background-color: #F2DFC8;
-		overflow: hidden;
+	.oxygen-container {
 		position: absolute;
 		top: 1rem;
-		width: 1.2rem;
-		border: 0.1rem solid #C2B2A0;
 
-		.progress {
-			@include inset(0);
-			background: linear-gradient(
-				to right,
-				#7F9BFE 0%,
-				#435BAA 100%
-			);
-			transform-origin: bottom;
-			transition: transform 0s;
-			border-radius: 1rem;
-		}
-
-		&.player-1 {
+		&:has(.player-1) {
 			left: 1rem;
 		}
 
-		&.player-2 {
+		&:has(.player-2) {
 			right: 1rem;
+		}
+
+		.oxygen-jauge {
+			border-radius: 1rem;
+			height: 15rem;
+			background-color: #F2DFC8;
+			overflow: hidden;
+			position: relative;
+			display: block;
+			top: 1rem;
+			width: 1.2rem;
+			margin: 0 auto;
+			border: 0.1rem solid #C2B2A0;
+
+			.progress {
+				position: absolute;
+				@include inset(0);
+				background: linear-gradient(
+					to right,
+					#7F9BFE 0%,
+					#435BAA 100%
+				);
+				transform-origin: bottom;
+				transition: transform 0s;
+				border-radius: 1rem;
+			}
+		}
+
+		.oxygen-icon {
+			position: relative;
+			display: block;
+			height: auto;
+			margin: 0 auto;
+			margin-top: 0.5rem;
+			width:	1.5rem
 		}
 	}
 </style>
