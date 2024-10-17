c159f8ad52831e6d4d4d09aa07fdea49ece9486d
Amandine Quellec <quellec.amandine@gmail.com>
2024-10-17 14:13:10 +0200

    feat: add ingredient class

--- src/game/InputSet.js
+++ src/game/InputSet.js
@@ -1,79 +1,79 @@
-import Axis from "axis-api";
-export default class InputSet{
+import Axis from "axis-api"
+export default class InputSet {
 	constructor(idPlayer) {
-		this.idPlayer = idPlayer;
-		this.currentPlayer = idPlayer === 1 ? InputSet.player1 : InputSet.player2;
+		this.idPlayer = idPlayer
+		this.currentPlayer = idPlayer === 1 ? InputSet.player1 : InputSet.player2
 	}
 
-	addEvent(key,callback,context, event = "keydown") {
-		const buttonA = Axis.buttonManager.getButton(key, this.idPlayer);
-		buttonA.addEventListener(event, callback.bind(context));
+	addEvent(key, callback, context, event = "keydown") {
+		const buttonA = Axis.buttonManager.getButton(key, this.idPlayer)
+		buttonA.addEventListener(event, callback.bind(context))
 	}
 
-	removeEventA(key,callback,context,event = "keydown"){
-		const buttonA = Axis.buttonManager.getButton(key, this.idPlayer);
-		buttonA.removeEventA(event, callback.bind(context));
+	removeEventA(key, callback, context, event = "keydown") {
+		const buttonA = Axis.buttonManager.getButton(key, this.idPlayer)
+		buttonA.removeEventA(event, callback.bind(context))
 	}
 
-	addEventJoystick(callback,context) {
-		this.currentPlayer.addEventListener("joystick:move", callback.bind(context));
-    }
+	addEventJoystick(callback, context) {
+		this.currentPlayer.addEventListener("joystick:move", callback.bind(context))
+	}
 
-	static emulateKeyboard(){
+	static emulateKeyboard() {
 		//First Group
-		Axis.registerKeys("a","a",1)
-		Axis.registerKeys("z","x",1)
-		Axis.registerKeys("e","i",1)
-		Axis.registerKeys("r","s",1)
-		Axis.registerKeys("t","w",1)
+		Axis.registerKeys("a", "a", 1)
+		Axis.registerKeys("z", "x", 1)
+		Axis.registerKeys("e", "i", 1)
+		Axis.registerKeys("r", "s", 1)
+		Axis.registerKeys("t", "w", 1)
 
 		//Second Group
-		Axis.registerKeys("u","a",2)
-		Axis.registerKeys("i","x",2)
-		Axis.registerKeys("o","i",2)
-		Axis.registerKeys("p","s",2)
-		Axis.registerKeys("^","w",2)
+		Axis.registerKeys("u", "a", 2)
+		Axis.registerKeys("i", "x", 2)
+		Axis.registerKeys("o", "i", 2)
+		Axis.registerKeys("p", "s", 2)
+		Axis.registerKeys("^", "w", 2)
 	}
 
-	static emulateGamePad(){
-		InputSet.gamepadEmulatorPlayer1 = Axis.createGamepadEmulator(0); // 0 is gamepad index, often represents the first gamepad connected to your computer
-		InputSet.gamepadEmulatorPlayer2 = Axis.createGamepadEmulator(1); // 0 is gamepad index, often represents the first gamepad connected to your computer
+	static emulateGamePad() {
+		InputSet.gamepadEmulatorPlayer1 = Axis.createGamepadEmulator(0) // 0 is gamepad index, often represents the first gamepad connected to your computer
+		InputSet.gamepadEmulatorPlayer2 = Axis.createGamepadEmulator(1) // 0 is gamepad index, often represents the first gamepad connected to your computer
 
-		Axis.joystick1.setGamepadEmulatorJoystick(InputSet.gamepadEmulatorPlayer1, 0); // 0 is the joystick index of the gamepad, often the one on the left side
-		Axis.joystick2.setGamepadEmulatorJoystick(InputSet.gamepadEmulatorPlayer2, 0); // 0 is the joystick index of the gamepad, often the one on the left side
+		Axis.joystick1.setGamepadEmulatorJoystick(InputSet.gamepadEmulatorPlayer1, 0) // 0 is the joystick index of the gamepad, often the one on the left side
+		Axis.joystick2.setGamepadEmulatorJoystick(InputSet.gamepadEmulatorPlayer2, 0) // 0 is the joystick index of the gamepad, often the one on the left side
 
-		Axis.registerGamepadEmulatorKeys(InputSet.gamepadEmulatorPlayer1, 0, "a", 1); // Gamepad button index 0 (PS4 X) to button "a" from group 1
-		Axis.registerGamepadEmulatorKeys(InputSet.gamepadEmulatorPlayer1, 1, "x", 1); // Gamepad button index 1 (PS4 Square) to button "x" from group 1
-		Axis.registerGamepadEmulatorKeys(InputSet.gamepadEmulatorPlayer1, 2, "i", 1); // Gamepad button index 2 (PS4 Circle) to button "i" from group 1
-		Axis.registerGamepadEmulatorKeys(InputSet.gamepadEmulatorPlayer1, 3, "s", 1); // Gamepad button index 3 (PS4 Triangle) to button "s" from group 1
+		Axis.registerGamepadEmulatorKeys(InputSet.gamepadEmulatorPlayer1, 0, "a", 1) // Gamepad button index 0 (PS4 X) to button "a" from group 1
+		Axis.registerGamepadEmulatorKeys(InputSet.gamepadEmulatorPlayer1, 1, "x", 1) // Gamepad button index 1 (PS4 Square) to button "x" from group 1
+		Axis.registerGamepadEmulatorKeys(InputSet.gamepadEmulatorPlayer1, 2, "i", 1) // Gamepad button index 2 (PS4 Circle) to button "i" from group 1
+		Axis.registerGamepadEmulatorKeys(InputSet.gamepadEmulatorPlayer1, 3, "s", 1) // Gamepad button index 3 (PS4 Triangle) to button "s" from group 1
 
-		Axis.registerGamepadEmulatorKeys(InputSet.gamepadEmulatorPlayer2, 0, "a", 1); // Gamepad button index 0 (PS4 X) to button "a" from group 1
-		Axis.registerGamepadEmulatorKeys(InputSet.gamepadEmulatorPlayer2, 1, "x", 1); // Gamepad button index 1 (PS4 Square) to button "x" from group 1
-		Axis.registerGamepadEmulatorKeys(InputSet.gamepadEmulatorPlayer2, 2, "i", 1); // Gamepad button index 2 (PS4 Circle) to button "i" from group 1
-		Axis.registerGamepadEmulatorKeys(InputSet.gamepadEmulatorPlayer2, 3, "s", 1); // Gamepad button index 3 (PS4 Triangle) to button "s" from group 1
+		Axis.registerGamepadEmulatorKeys(InputSet.gamepadEmulatorPlayer2, 0, "a", 1) // Gamepad button index 0 (PS4 X) to button "a" from group 1
+		Axis.registerGamepadEmulatorKeys(InputSet.gamepadEmulatorPlayer2, 1, "x", 1) // Gamepad button index 1 (PS4 Square) to button "x" from group 1
+		Axis.registerGamepadEmulatorKeys(InputSet.gamepadEmulatorPlayer2, 2, "i", 1) // Gamepad button index 2 (PS4 Circle) to button "i" from group 1
+		Axis.registerGamepadEmulatorKeys(InputSet.gamepadEmulatorPlayer2, 3, "s", 1) // Gamepad button index 3 (PS4 Triangle) to button "s" from group 1
 	}
 
-	static initPlayersInputs(){
+	static initPlayersInputs() {
 		InputSet.player1 = Axis.createPlayer({
 			id: 1,
 			joysticks: Axis.joystick1,
 			buttons: Axis.buttonManager.getButtonsById(1), // Give player 1 all buttons from group 1
-		});
+		})
 
 
 		InputSet.player2 = Axis.createPlayer({
 			id: 2,
 			joysticks: Axis.joystick2,
 			buttons: Axis.buttonManager.getButtonsById(2), // Give player 1 all buttons from group 2
-		});
+		})
 	}
 
-	static update(){
-		if(InputSet.gamepadEmulatorPlayer1){
-			InputSet.gamepadEmulatorPlayer1.update();
+	static update() {
+		if (InputSet.gamepadEmulatorPlayer1) {
+			InputSet.gamepadEmulatorPlayer1.update()
 		}
-		if(InputSet.gamepadEmulatorPlayer2){
-			InputSet.gamepadEmulatorPlayer2.update();
+		if (InputSet.gamepadEmulatorPlayer2) {
+			InputSet.gamepadEmulatorPlayer2.update()
 		}
 	}
 }
---
+++ src/game/recipe/IngredientManager.js
@@ -0,0 +1,167 @@
+import { Game } from "../Game.js"
+import Ingredient from "./Ingredient.js"
+
+export default class IngredientManager {
+	#ingredients
+	#ingredientsToSpawn
+	#player1HasFinishedTheRecipe
+	#player2HasFinishedTheRecipe
+	#game
+	#recipes
+	#ingredientsSpawned
+	#lastSpawnTime
+
+	constructor(recipes) {
+		this.#game = new Game()
+		this.#recipes = recipes
+		this.#ingredients = []
+		this.#ingredientsToSpawn = {}
+		this.#ingredientsSpawned = {}
+		this.#player1HasFinishedTheRecipe = false
+		this.#player2HasFinishedTheRecipe = false
+		this.#lastSpawnTime = {}
+	}
+
+	async spawnIngredient() {
+		const ingredientNames = Object.keys(this.#ingredientsToSpawn)
+		if (ingredientNames.length > 0) {
+			const currentTime = Date.now()
+			const spawnableIngredients = ingredientNames.filter(name => {
+				const recipeIngredient = this.#recipes.flatMap(recipe => recipe.ingredients).find(ingredient => ingredient.name === name)
+				if (!recipeIngredient) {
+					return false
+				}
+				const lastSpawnTime = this.#lastSpawnTime[ name ] || 0
+				return currentTime - lastSpawnTime >= recipeIngredient.spawnRate
+			})
+
+			if (spawnableIngredients.length > 0) {
+				const randomIndex = Math.floor(Math.random() * spawnableIngredients.length)
+				const ingredientName = spawnableIngredients[ randomIndex ]
+
+				const recipeIngredient = this.#recipes.flatMap(recipe => recipe.ingredients).find(ingredient => ingredient.name === ingredientName)
+				if (!recipeIngredient) {
+					return null
+				}
+
+				const x = Math.random() * window.innerWidth
+
+				const ingredient = new Ingredient(this, recipeIngredient.name, recipeIngredient.texture, x, recipeIngredient.canMove, recipeIngredient.action, recipeIngredient.isCooked)
+				await ingredient.create()
+				this.#ingredientsSpawned[ ingredientName ] = (this.#ingredientsSpawned[ ingredientName ] || 0) + 1
+				this.#ingredientsToSpawn[ ingredientName ]--
+				this.#lastSpawnTime[ ingredientName ] = currentTime
+				if (this.#ingredientsToSpawn[ ingredientName ] !== undefined && this.#ingredientsToSpawn[ ingredientName ] === 0) {
+					delete this.#ingredientsToSpawn[ ingredientName ]
+				}
+				return ingredient
+			}
+		}
+		return null
+	}
+
+	init() {
+		this.createIngredients()
+	}
+
+	addIngredient(ingredient) {
+		this.#ingredients.push(ingredient)
+	}
+
+	removeIngredient(ingredient) {
+		const index = this.#ingredients.indexOf(ingredient)
+		if (index > -1) {
+			this.#ingredients.splice(index, 1)
+		}
+
+		const ingredientName = ingredient.getName()
+		if (this.#ingredientsSpawned[ ingredientName ] !== undefined) {
+			this.#ingredientsSpawned[ ingredientName ]--
+			if (this.#ingredientsSpawned[ ingredientName ] === 0) {
+				delete this.#ingredientsSpawned[ ingredientName ]
+			}
+		}
+	}
+
+	update(dt) {
+		if (Object.keys(this.#ingredientsToSpawn).length > 0) {
+			if (!this.#player1HasFinishedTheRecipe || !this.#player2HasFinishedTheRecipe) {
+				this.spawnIngredient()
+			}
+		}
+
+		this.#ingredients.forEach((ingredient) => {
+			ingredient.update(dt)
+		})
+
+		console.log(this.#ingredientsSpawned)
+	}
+
+	destroy() {
+		this.#ingredientsToSpawn = {}
+		this.#player1HasFinishedTheRecipe = false
+		this.#player2HasFinishedTheRecipe = false
+		this.#ingredientsSpawned = {}
+		this.#lastSpawnTime = {}
+	}
+
+	getIngredients() {
+		return this.#ingredients
+	}
+
+	getIngredientsToSpawn() {
+		return this.#ingredientsToSpawn
+	}
+
+	getPlayer1HasFinishedTheRecipe() {
+		return this.#player1HasFinishedTheRecipe
+	}
+
+	getPlayer2HasFinishedTheRecipe() {
+		return this.#player2HasFinishedTheRecipe
+	}
+
+	getIngredientsSpawned() {
+		return this.#ingredientsSpawned
+	}
+
+	setIngredientsToSpawn(ingredientsToSpawn) {
+		this.#ingredientsToSpawn = ingredientsToSpawn
+	}
+
+	setPlayer1HasFinishedTheRecipe(player1HasFinishedTheRecipe) {
+		this.#player1HasFinishedTheRecipe = player1HasFinishedTheRecipe
+		this.updateIngredientsToSpawn()
+	}
+
+	setPlayer2HasFinishedTheRecipe(player2HasFinishedTheRecipe) {
+		this.#player2HasFinishedTheRecipe = player2HasFinishedTheRecipe
+		this.updateIngredientsToSpawn()
+	}
+
+	setIngredientsSpawned(ingredientsSpawned) {
+		this.#ingredientsSpawned = ingredientsSpawned
+	}
+
+	createIngredients() {
+		this.updateIngredientsToSpawn()
+	}
+
+	updateIngredientsToSpawn() {
+		const multiplier = this.#player1HasFinishedTheRecipe || this.#player2HasFinishedTheRecipe ? 1 : 2
+
+		if (!Array.isArray(this.#recipes)) {
+			this.#recipes = [ this.#recipes ]
+		}
+
+		if (this.#recipes && this.#recipes.length > 0) {
+			this.#recipes.forEach((recipe) => {
+				if (recipe.ingredients && Array.isArray(recipe.ingredients)) {
+					recipe.ingredients.forEach((ingredient) => {
+						this.#ingredientsToSpawn[ ingredient.name ] = (this.#ingredientsToSpawn[ ingredient.name ] || 0) + ingredient.quantity * multiplier
+					})
+				}
+			})
+		}
+	}
+}
