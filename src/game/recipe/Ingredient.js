97ad8e4abe17288506376cf3ffbb0802a4c5dcb6
Amandine Quellec <quellec.amandine@gmail.com>
2024-10-17 15:14:52 +0200

    feat: add spritesheet

--- src/game/recipe/Ingredient.js
+++ src/game/recipe/Ingredient.js
@@ -1,6 +1,7 @@
 import { v4 as uuidv4 } from "uuid"
-import PixiSprite from "@/game/pixi/PixiSprite"
 import PixiApplication from "@/game/pixi/PixiApplication"
+import { Assets, AnimatedSprite } from "pixi.js"
+
 
 export default class Ingredient {
 	#id
@@ -10,29 +11,49 @@ export default class Ingredient {
 	#game
 	#action
 	#isCooked
+	#nbOfFrames = 0
 
-	constructor(ref, name, texture, x, canMove = true, action, isCooked = false) {
+	constructor(ref, name, spritesheet, atlasData, animationName, size, x, canMove = true, action, isCooked = false) {
 		this.ref = ref
 		this.#id = uuidv4()
 		this.#name = name
-		this.texture = texture
+		this.spritesheet = spritesheet
+		this.atlasData = atlasData
+		this.animationName = animationName
+		this.size = size
 		this.x = x
 		// this.#game = new Game()
-		// this.#sprite = new Sprite(texture);
+		// this.#sprite = new Sprite(spritesheet);
 		this.#canMove = canMove
 		// this.#action = new Action()
 		this.#isCooked = isCooked
 	}
 
+	setAnimatedSpriteFrame(frame) {
+		this.sprite.gotoAndStop(frame)
+	}
+
 	async initPixiSprite() {
-		try {
-			const pixiApplication = new PixiApplication()
-			this.pixiSprite = new PixiSprite(this.texture, this.x, 0)
-			const sprite = await this.pixiSprite.init()
-			pixiApplication.appendToStage(sprite)
-		} catch (error) {
-			console.error("Error initializing PixiSprite:", error)
-		}
+		if (!this.spritesheet || !this.atlasData || !this.animationName) return
+		const pixiApplication = new PixiApplication()
+
+		const sheetTexture = await Assets.load(this.spritesheet)
+		Assets.add({
+			alias: `atlas-${ this.#name }`,
+			src: this.atlasData,
+			data: { texture: sheetTexture }
+		})
+
+		const sheet = await Assets.load(`atlas-${ this.#name }`)
+
+		this.sprite = new AnimatedSprite(sheet.animations[ this.animationName ])
+
+		this.sprite.scale = this.size
+
+		this.sprite.x = this.x
+		this.sprite.y = 0
+
+		pixiApplication.appendToStage(this.sprite)
 	}
 
 	async create() {
@@ -45,13 +66,22 @@ export default class Ingredient {
 	}
 
 	update(dt) {
-		if (this.pixiSprite) {
-			if (this.pixiSprite?.sprite?.position) {
-				this.pixiSprite.sprite.position.y += 1
+		// if (this.pixiSprite) {
+		// 	if (this.pixiSprite?.sprite?.position) {
+		// 		this.pixiSprite.sprite.position.y += 1
 
-				if (this.pixiSprite.sprite.position.y > window.innerHeight) {
-					this.destroy()
-				}
+		// 		if (this.pixiSprite.sprite.position.y > window.innerHeight) {
+		// 			this.destroy()
+		// 		}
+		// 	}
+		// }
+
+		if (this.sprite) {
+			console.log(this.sprite.position)
+			this.sprite.position.y += 1
+
+			if (this.sprite.position.y > window.innerHeight) {
+				this.destroy()
 			}
 		}
 	}
