02d61e8c6b72ffc7cf2789bd4d486d8a5d551b22
flanb <clement.renou.47@gmail.com>
2024-10-21 23:06:42 +0200

    add background and post process

--- src/game/pixi/PixiApplication.js
+++ src/game/pixi/PixiApplication.js
@@ -1,4 +1,5 @@
-import { Application } from "pixi.js"
+import * as PIXI from "pixi.js"
+import {ZoomBlurFilter} from "pixi-filters";
 
 export default class PixiApplication {
 	static instance
@@ -10,12 +11,43 @@ export default class PixiApplication {
 		console.log("init")
 	}
 
-	async init(wrapper, color = "#da0000") {
-		this.app = new Application()
-		await this.app.init({ backgroundAlpha: 0, resizeTo: window }).then(() => {
+	async init(wrapper, color = "#000") {
+		this.app = new PIXI.Application()
+		await this.app.init({ resizeTo: window, }).then(() => {
 			wrapper.appendChild(this.app.canvas)
 			this.canvas = this.app.canvas
 		})
+
+		await PIXI.Assets.load("/assets/img/office.webp");
+		// change background with an image /assets/img/office.webp
+		const assets = PIXI.Assets.get("/assets/img/office.webp")
+		// get ratio of image
+		const ratio = assets.width / assets.height
+		const slide = new PIXI.Sprite(assets)
+		slide.width = window.innerWidth * ratio
+		slide.height = window.innerHeight
+		slide.x = window.innerWidth / 2 - slide.width / 2
+		this.app.stage.addChild(slide)
+
+
+
+		// create custom filter
+
+
+		const filter = new ZoomBlurFilter({
+			strength: 0.01,
+		})
+
+		 this.app.stage.filters = [filter ]
+
+		const update = () => {
+
+			filter.strength = (Math.sin(Date.now() * 0.0001) + 0.5) * 0.1
+			requestAnimationFrame(update)
+		}
+		update()
+
+
 	}
 
 	appendToStage(elt) {
