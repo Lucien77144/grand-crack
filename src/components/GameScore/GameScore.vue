c629af2a6b5d2153bbe23c4f803ee09327f61005
Lucien <lucien.boisseau-sable@smice.com>
2024-10-24 22:02:23 +0200

    fix ui in progro

--- src/components/GameScore/GameScore.vue
+++ src/components/GameScore/GameScore.vue
@@ -4,21 +4,32 @@
 </script>
 
 <template>
-	<div
-		class="game-score"
-		:class="['player-' + player]"
-	>
-		<p v-html="store.players[player - 1].score" />
+	<div class="game-score__wrapper">
+		<div class="game-score" :class="['player-' + player]">
+			<p v-html="store.players[player - 1].score" />
+			<img class="score__bg" src="/assets/img/score.png" alt="">
+		</div>
 	</div>
 </template>
 
 <style lang="scss" scoped>
 	.game-score {
-		font-size: 4rem;
+		align-items: center;
 		color: white;
-		position: absolute;
-		top: 0;
+		display: flex;
+		font-family: DigitalNumbers, sans-serif;
+		font-size: 2rem;
 		transition: opacity 0.5s;
+
+		&__wrapper {
+			position: absolute;
+			top: 0;
+		}
+
+		p {
+			position: relative;
+			z-index: 1;
+		}
 
 		&.player-1 {
 			left: 10rem;
@@ -27,5 +38,13 @@
 		&.player-2 {
 			right: 10rem;
 		}
+	}
+
+	.score__bg {
+		height: auto;
+		left: 0;
+		position: absolute;
+		top: 0;
+		width: 8rem;
 	}
 </style>
